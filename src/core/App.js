e3e9df5cf8eefdd3eedb2a5e3e28d2131f421065
Fikol <pawelowiesek@gmail.com>
2023-08-21 15:45:30 +0200

    add RequestSection and Section

--- src/core/App.js
+++ src/core/App.js
@@ -1,7 +1,16 @@
 import Header from "../common/Header";
+import RequestSection from "../common/RequestSection";
+import Section from "../common/Section";
+import Main from "../features/Main";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <RequestSection />
+      <Section title="Why choose Easybank?" body={<Main />} />
+    </>
+  );
 }
 
 export default App;
