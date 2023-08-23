33db651a2fce33da5a477134efa022760e9279a7
Fikol <pawelowiesek@gmail.com>
2023-08-23 07:31:13 +0200

    add new styled-components

--- src/common/Tile/index.js
+++ src/common/Tile/index.js
@@ -1,9 +1,12 @@
-const Tile = ({ icon, title, content }) => {
+import { Content, SubTitle } from "./styled";
+
+const Tile = ({ icon, title, content, subTitle }) => {
   return (
     <div>
       <p>{icon}</p>
+      <SubTitle>{subTitle}</SubTitle>
       <p>{title}</p>
-      <p>{content}</p>
+      <Content>{content}</Content>
     </div>
   );
 };
