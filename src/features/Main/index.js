1da5e4a0e961ea42b9f31a681106659eb7bd48a3
Fikol <pawelowiesek@gmail.com>
2023-08-23 07:32:14 +0200

    add styled-components

--- src/features/Main/index.js
+++ src/features/Main/index.js
@@ -2,17 +2,15 @@ import Tile from "../../common/Tile";
 import {
   ApiIcon,
   BudgetIcon,
-  Container,
   Info,
   OnBoardingIcon,
   OnlineIcon,
-  Text,
   Wrapper,
 } from "./styled";
 
 const Main = () => {
   return (
-    <Container>
+    <>
       <Info>
         We leverage Open Banking to turn your bank account into your financial
         hub.
@@ -22,45 +20,29 @@ const Main = () => {
         <Tile
           icon={<OnlineIcon />}
           title="Online Banking"
-          content={
-            <Text>
-              Our modern web and mobile applications allow you to keep track of
-              your finances wherever you are in the world.
-            </Text>
-          }
+          content="Our modern web and mobile applications allow you to keep track of
+              your finances wherever you are in the world."
         />
         <Tile
           icon={<BudgetIcon />}
           title="Simple Budgeting"
-          content={
-            <Text>
-              See exactly where your money goes each month. Receive
-              notifications when you’re close to hitting your limits
-            </Text>
-          }
+          content="See exactly where your money goes each month. Receive
+              notifications when you’re close to hitting your limits"
         />
         <Tile
           icon={<OnBoardingIcon />}
           title="Fast Onboarding"
-          content={
-            <Text>
-              We don’t do branches. Open your account in minutes online and
-              start taking control of your finances right away.{" "}
-            </Text>
-          }
+          content="We don’t do branches. Open your account in minutes online and
+              start taking control of your finances right away."
         />
         <Tile
           icon={<ApiIcon />}
           title="Open API"
-          content={
-            <Text>
-              Manage your savings, investments, pension, and much more from one
-              account. Tracking your money has never been easier.{" "}
-            </Text>
-          }
+          content="Manage your savings, investments, pension, and much more from one
+              account. Tracking your money has never been easier."
         />
       </Wrapper>
-    </Container>
+    </>
   );
 };
 export default Main;
