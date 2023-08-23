b9674480bf8cc1059f3824f95c14e44e40083775
Fikol <pawelowiesek@gmail.com>
2023-08-23 07:31:43 +0200

    add Section and Footer components

--- src/core/App.js
+++ src/core/App.js
@@ -2,6 +2,8 @@ import Header from "../common/Header";
 import RequestSection from "../common/RequestSection";
 import Section from "../common/Section";
 import Main from "../features/Main";
+import SubSection from "../features/SubSection";
+import Footer from "../common/Footer";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Header />
       <RequestSection />
       <Section title="Why choose Easybank?" body={<Main />} />
+      <Section title="Latest Articles" body={<SubSection />} />
+      <Footer />
     </>
   );
 }
